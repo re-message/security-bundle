4098ab6eadb4d28bdf631b84297b4c66e5d5aff4
Oleg Kozlov <h1karo@relmsg.ru>
2022-01-25 09:18:21 +0300

    Implement subject provider interface

---
+++ src/Provider/AbstractSubjectProvider.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Security Bundle.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/security-bundle
+ * @link      https://dev.relmsg.ru/packages/security-bundle
+ * @copyright Copyright (c) 2018-2022 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/security-bundle
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Bundle\JwtSecurityBundle\Provider;
+
+use RM\Bundle\JwtSecurityBundle\Entity\SubjectInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+abstract class AbstractSubjectProvider implements SubjectProviderInterface
+{
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        return $user;
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return is_subclass_of($class, SubjectInterface::class, true);
+    }
+}
---
+++ src/Provider/SubjectProviderInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Security Bundle.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/security-bundle
+ * @link      https://dev.relmsg.ru/packages/security-bundle
+ * @copyright Copyright (c) 2018-2022 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/security-bundle
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Bundle\JwtSecurityBundle\Provider;
+
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+interface SubjectProviderInterface extends UserProviderInterface
+{
+}
