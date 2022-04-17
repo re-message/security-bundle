df397922ad0a8bdd1ed2ad3d01d467d5d91e6038
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-17 19:45:16 +0300

    Configure phpunit

---
+++ tests/Functional/Kernel.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Security Bundle.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/security-bundle
+ * @link      https://dev.relmsg.ru/packages/security-bundle
+ * @copyright Copyright (c) 2018-2022 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/security-bundle
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Bundle\JwtSecurityBundle\Tests\Functional;
+
+use Exception;
+use RM\Bundle\JwtSecurityBundle\JwtSecurityBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+/**
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+class Kernel extends BaseKernel
+{
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    private ?string $testCase;
+
+    public function __construct(string $environment, bool $debug, string $testCase = null)
+    {
+        parent::__construct($environment, $debug);
+
+        $this->testCase = $testCase;
+    }
+
+    public function registerBundles(): iterable
+    {
+        return [
+            new FrameworkBundle(),
+            new SecurityBundle(),
+            new MonologBundle(),
+            new JwtSecurityBundle(),
+        ];
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/JwtSecurityBundle/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/JwtSecurityBundle/log';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $confDir = $this->getProjectDir() . '/config';
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+
+        $testCaseDir = implode('/', [$confDir, $this->testCase]);
+        if ($this->testCase && file_exists($testCaseDir) && is_dir($testCaseDir)) {
+            $loader->load($testCaseDir . '/*' . self::CONFIG_EXTS, 'glob');
+        }
+    }
+}
