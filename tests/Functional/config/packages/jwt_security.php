54f50210e2db212a6aa00a239008aad4cc27b828
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-19 23:41:09 +0300

    Register property validators in test env

--- tests/Functional/config/packages/jwt_security.php
+++ tests/Functional/config/packages/jwt_security.php
@@ -1,6 +1,34 @@
 <?php
 
+use RM\Standard\Jwt\Validator\Property\ExpirationValidator;
+use RM\Standard\Jwt\Validator\Property\IdentifierValidator;
+use RM\Standard\Jwt\Validator\Property\IssuedAtValidator;
+use RM\Standard\Jwt\Validator\Property\IssuerValidator;
+use RM\Standard\Jwt\Validator\Property\NotBeforeValidator;
 use Symfony\Config\JwtSecurityConfig;
 
 return static function (JwtSecurityConfig $config) {
+    $config->propertyValidator()
+        ->class(IdentifierValidator::class)
+    ;
+
+    $config->propertyValidator()
+        ->class(ExpirationValidator::class)
+        ->arguments(['leeway' => 30])
+    ;
+
+    $config->propertyValidator()
+        ->class(IssuedAtValidator::class)
+        ->arguments(['leeway' => 30])
+    ;
+
+    $config->propertyValidator()
+        ->class(NotBeforeValidator::class)
+        ->arguments(['leeway' => 30])
+    ;
+
+    $config->propertyValidator()
+        ->class(IssuerValidator::class)
+        ->arguments(['issuers' => ['relmsg/security-bundle']])
+    ;
 };
